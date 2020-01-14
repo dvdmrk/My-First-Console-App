e02e6eaf795f16d8d7ed36548102091cf7feb3ad
dvdmrk <davidmerk25@gmail.com>
2020-01-14 12:33:45 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace My_First_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
